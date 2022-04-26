8038fa13fdd0c41e90d77b3a629fe827ff6d51e8
Fred <felick4000@gmail.com>
2022-04-26 17:45:42 -0400

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,27 +9,46 @@ console.log (playerName, playerAttack, playerHealth);
 var enemyName = "Roborto";
 var enemyHealth  = 50;
 var enemyAttack = 12;
+var playerMoney =10;
 
 var fight= function(){
     window.alert("Welcome to Robot Gladiators!");
+    var promptFight=window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
+
+    if(promptFight === "fight" || promptFight === "FIGHT"){
     enemyHealth=enemyHealth-playerAttack;
      console.log(
          playerName+" attacked"+enemyName+ ". "+enemyName + " now has "+ enemyHealth+ " health remaining"
      );
+             //check enemy health
+             if (enemyHealth <= 0) {
+                window.alert(enemyName + " has died!");
+            }
+            else{
+                window.alert(enemyName+ " still has "+ enemyHealth+ " health left.");
+            }
 
      playerHealth=playerHealth-enemyAttack;
      console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
-
-        //check enemy health
-        if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
+        if (playerHealth <= 0) {
+            window.alert(playerName + " has died!");
+          }
+          else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+          }
+        } else if (promptFight === "skip" || promptFight === "SKIP"){
+            var confirmSkip= window.confirm("Are you sure you'd like to quit?");
+            if(confirmSkip){
+                playerMoney=playerMoney-2;
+            }
+            else{
+                fight();
+            }
+            window.alert(playerName+ " has chosen to skip the fight!");
+        } else{
+            window.alert("You need to choose a valid option. Try again!")
         }
-        else{
-            window.alert(enemyName+ " still has "+ enemyHealth+ " health left.");
-        }
-
-
 
 }
 
