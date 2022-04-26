0294e85fabaefa3fba699902bf3c275d25765799
Fred <felick4000@gmail.com>
2022-04-26 17:27:39 -0400

    first battle

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,36 @@
-window.alert("This is an alert! Javascript is running!")
+var playerName=window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+//you can also log multiple values at once like this
+
+console.log (playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth  = 50;
+var enemyAttack = 12;
+
+var fight= function(){
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth=enemyHealth-playerAttack;
+     console.log(
+         playerName+" attacked"+enemyName+ ". "+enemyName + " now has "+ enemyHealth+ " health remaining"
+     );
+
+     playerHealth=playerHealth-enemyAttack;
+     console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+        //check enemy health
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died!");
+        }
+        else{
+            window.alert(enemyName+ " still has "+ enemyHealth+ " health left.");
+        }
+
+
+
+}
+
+fight();
